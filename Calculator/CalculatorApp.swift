54d5028e30a24458fd5954c87490f5553eb639b1
Moamen Hassaballah <hassaballahmoamen84@gmail.com>
2024-07-29 20:53:40 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Moamen Hassaballah on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
